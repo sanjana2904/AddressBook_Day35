740666d47b3c0bba9c41cd250a287a2018065d7e
sanjana k <sanjanarthi@gmail.com>
2021-04-28 12:57:09 +0530

    Added Creating_NewAddressBook_Array.js

---
+++ AddressBookSystemUsing_JS/Creating_NewAddressBook_Array.js
@@ -0,0 +1,7 @@
+//UC3
+let addressBook2 = new AddressBook("Arthi", "K", "sara Nagar", "Chennai", "TamilNadu", 635003, 9988776611, "ccc123@gmail.com");
+let addressBookArray = new Array();
+addressBookArray.push(addressBook);
+addressBookArray.push(addressBook1);
+addressBookArray.push(addressBook2);
+console.log("\nNew Contact Added to Array " + addressBookArray);
